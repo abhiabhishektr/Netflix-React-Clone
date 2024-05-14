b08368f95e682ac23762795546b82286eca7219a
abhishek <abhiabhishektr@gmail.com>
2024-05-14 18:35:56 +0530

    almost completed

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { AuthContextProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
+import ProtectedRoute from './components/ProtectedRoute';
+
+// Load environment variables
+// require('dotenv').config();
+
+function App() {
+  return (  
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </>
+  );
+}
+
+export default App;
